1e56886262cb23a1fd964ccbbe6f3648a7739b2b
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-06-27 21:12:09 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
 import "./styles/App.css";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Shop" element={<Shop />}></Route>
+        <Route path="/Cart" element={<Cart />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
