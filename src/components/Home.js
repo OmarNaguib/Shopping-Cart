aff0cd2d57d49a6cfb110047e0f0773355c687a7
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-06-28 23:10:03 +0300

    Layout Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="home-message">
+        <div className="text">
+          Athletes are cool <br /> But some are just{" "}
+          <span className="special-text">Exceptional</span>
+        </div>
+      </div>
+    </div>
+  );
 }
