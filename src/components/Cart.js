9dd2c03386f50c3a6846923936ebe77747c5f497
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-02 22:31:39 +0300

    Extract CheckItems

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,34 +1,13 @@
 import LabelRow from "./LabelRow";
+import CheckItems from "./CheckItems";
 
-export default function Cart({ removeFromCart, increment, decrement, items }) {
-  const checkItems = items
-    .filter((item) => item.inCart)
-    .map((item, index) => {
-      return (
-        <div className="checkout-item">
-          <div className="product">
-            <img src={item.src} alt="" />
-            <div className="name">{item.name}</div>
-          </div>
-          <div className="price">£{item.price}</div>
-          <div className="quantity">
-            <div className="number">{item.inCart}</div>
-            <div className="buttons">
-              <button onClick={increment.bind(null, item.id)}>+</button>
-              <button onClick={decrement.bind(null, item.id)}>-</button>
-            </div>
-          </div>
-          <div className="subtotal">£{item.price * item.inCart}</div>
-          <button onClick={removeFromCart.bind(null, item.id)}>delete</button>
-        </div>
-      );
-    });
+export default function Cart(props) {
   return (
     <div className="cart">
       <div className="checkout-card">
         <h1>Cart</h1>
         <LabelRow></LabelRow>
-        {checkItems}
+        <CheckItems {...props}></CheckItems>
         <button className="checkout">Checkout</button>
       </div>
     </div>
