07fd858e55434260dada21881dd0ad1f40585dc3
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-02 22:26:27 +0300

    Layout cart items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,23 +1,36 @@
+import LabelRow from "./LabelRow";
+
 export default function Cart({ removeFromCart, increment, decrement, items }) {
   const checkItems = items
     .filter((item) => item.inCart)
     .map((item, index) => {
       return (
         <div className="checkout-item">
-          <img src={item.src} alt="" />
-          <div className="name">{item.name}</div>
+          <div className="product">
+            <img src={item.src} alt="" />
+            <div className="name">{item.name}</div>
+          </div>
           <div className="price">£{item.price}</div>
-          <div className="quantity">£{item.inCart}</div>
-          <button onClick={increment.bind(null, item.id)}>+</button>
-          <button onClick={decrement.bind(null, item.id)}>-</button>
+          <div className="quantity">
+            <div className="number">{item.inCart}</div>
+            <div className="buttons">
+              <button onClick={increment.bind(null, item.id)}>+</button>
+              <button onClick={decrement.bind(null, item.id)}>-</button>
+            </div>
+          </div>
+          <div className="subtotal">£{item.price * item.inCart}</div>
           <button onClick={removeFromCart.bind(null, item.id)}>delete</button>
-          {item.inCart ? "incart" : "not"}
         </div>
       );
     });
   return (
     <div className="cart">
-      <div className="checkout-card">{checkItems}</div>
+      <div className="checkout-card">
+        <h1>Cart</h1>
+        <LabelRow></LabelRow>
+        {checkItems}
+        <button className="checkout">Checkout</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/LabelRow.js
@@ -0,0 +1,10 @@
+export default function LabelRow() {
+  return (
+    <div className="label-row">
+      <div className="product">Product</div>
+      <div className="price">Price</div>
+      <div className="quantity">Quantity</div>
+      <div className="subtotal">subtotal</div>
+    </div>
+  );
+}
