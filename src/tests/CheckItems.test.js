22f9fe05c886044e94ffc0ad76f2454b0c9174f5
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-04 02:22:49 +0300

    Test conditional rendering in Cart

--- src/tests/CheckItems.test.js
+++ src/tests/CheckItems.test.js
@@ -10,13 +10,13 @@ function setup() {
       src: "path/to/image",
       name: "JOOLA",
       price: "599.00",
-      inCart: 1,
+      inCart: 0,
     },
     {
       src: "path/to/image",
       name: "table",
       price: "50.00",
-      inCart: 0,
+      inCart: 1,
     },
     {
       src: "path/to/image",
@@ -32,3 +32,20 @@ function setup() {
 
   return { items, removeFromCart, increment, decrement };
 }
+
+describe("When displaying cart items", () => {
+  it("displays only inCart items", () => {
+    const { items, removeFromCart, increment, decrement } = setup();
+    const { container } = render(
+      <CheckItems
+        items={items}
+        removeFromCart={removeFromCart}
+        increment={increment}
+        decrement={decrement}
+      />
+    );
+    expect(screen.queryAllByTestId("checkout-item").length).toBe(2);
+    expect(screen.queryAllByTestId("name").length).toBe(2);
+    expect(screen.queryAllByTestId("name")[0].textContent).toBe("table");
+  });
+});
