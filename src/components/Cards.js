2077d6f8b4a62dc2e903114f1df44a977dcab2f5
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-03 22:45:24 +0300

    Add test ids to shop elements

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,18 @@
 export default function Cards({ items, addToCart }) {
   const cards = items.map((item, index) => (
-    <div className="card">
+    <div className="card" data-testid="card">
       <div
         className="img"
         style={{
           backgroundImage: `url("${item.src}")`,
         }}
       ></div>
-      <div className="name">{item.name}</div>
-      <div className="price">£{item.price}</div>
+      <div className="name" data-testid="name">
+        {item.name}
+      </div>
+      <div className="price" data-testid="price">
+        £{item.price}
+      </div>
       <button
         onClick={addToCart.bind(null, index)}
         className={item.inCart ? "in-cart" : ""}
