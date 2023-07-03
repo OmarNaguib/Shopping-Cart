976f7838f00e92910a57dd66064aada4ffce16fc
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-03 23:25:08 +0300

    Implement setup

--- src/tests/Cards.test.js
+++ src/tests/Cards.test.js
@@ -3,29 +3,36 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Cards from "../components/Cards";
 
-const items = [
-  {
-    src: "path/to/image",
-    name: "JOOLA",
-    price: "599.00",
-    inCart: 0,
-  },
-  {
-    src: "path/to/image",
-    name: "table",
-    price: "50.00",
-    inCart: 0,
-  },
-  {
-    src: "path/to/image",
-    name: "pat",
-    price: "30.00",
-    inCart: 0,
-  },
-];
+function setup() {
+  const items = [
+    {
+      src: "path/to/image",
+      name: "JOOLA",
+      price: "599.00",
+      inCart: 0,
+    },
+    {
+      src: "path/to/image",
+      name: "table",
+      price: "50.00",
+      inCart: 0,
+    },
+    {
+      src: "path/to/image",
+      name: "pat",
+      price: "30.00",
+      inCart: 0,
+    },
+  ];
+
+  const addToCart = jest.fn();
+
+  return { items, addToCart };
+}
 
 describe("When displaying products", () => {
   it("renders accurate item info", () => {
+    const { items, addToCart } = setup();
     const { container } = render(<Cards items={items} addToCart={jest.fn()} />);
     expect(screen.queryAllByTestId("card").length).toBe(3);
     expect(screen.queryAllByTestId("name").length).toBe(3);
@@ -34,5 +41,11 @@ describe("When displaying products", () => {
     expect(screen.queryAllByTestId("price")[2].textContent).toBe("£30.00");
     expect(screen.queryAllByRole("button").length).toBe(3);
     expect(screen.queryAllByRole("button")[0].textContent).toBe("Add to Cart");
+  });
+
+  it("Behaves expectedly on cart addition", () => {
+    const { items, addToCart } = setup();
+
+    const { container } = render(<Cards items={items} addToCart={jest.fn()} />);
   });
 });
