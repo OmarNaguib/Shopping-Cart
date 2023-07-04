712806a63a2b4ef2fe48c224790d45af221f786c
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-05 02:56:41 +0300

    Wrap App updates in individual act() functions

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,7 @@ import { queryAllByRole, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
 import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 
 describe("App Integration test", () => {
   it("Displays items in shop", async () => {
@@ -10,7 +11,9 @@ describe("App Integration test", () => {
     const user = userEvent.setup();
 
     const toShop = screen.queryByRole("link", { name: "Shop" });
-    await user.click(toShop);
+    await act(async () => {
+      await user.click(toShop);
+    });
 
     expect(screen.queryAllByTestId("card").length).toBeGreaterThan(0);
   });
@@ -20,15 +23,22 @@ describe("App Integration test", () => {
     const user = userEvent.setup();
 
     const toShop = screen.queryByRole("link", { name: "Shop" });
-    await user.click(toShop);
+    await act(async () => {
+      await user.click(toShop);
+    });
     const buttons = screen.queryAllByRole("button", { name: "Add to Cart" });
-    await user.click(buttons[0]);
-    await user.click(buttons[2]);
-    await user.click(buttons[4]);
+    await act(async () => {
+      await user.click(buttons[0]);
+      await user.click(buttons[2]);
+      await user.click(buttons[4]);
+    });
 
     expect(screen.queryAllByRole("button", { name: "In Cart" }).length).toBe(3);
     const toCart = screen.queryByRole("link", { name: "Cart" });
-    await user.click(toCart);
+    await act(async () => {
+      await user.click(toCart);
+    });
+
     expect(screen.queryAllByTestId("checkout-item").length).toBe(3);
   });
 
@@ -37,15 +47,24 @@ describe("App Integration test", () => {
     const user = userEvent.setup();
 
     const toShop = screen.queryByRole("link", { name: "Shop" });
-    await user.click(toShop);
+    await act(async () => {
+      await user.click(toShop);
+    });
     const buttons = screen.queryAllByRole("button", { name: "Add to Cart" });
-    await user.click(buttons[0]);
-    const toCart = screen.queryByRole("link", { name: "Cart" });
-    await user.click(toCart);
+    await act(async () => {
+      await user.click(buttons[0]);
+    });
 
-    await user.click(screen.getByRole("button", { name: "+" }));
-    await user.click(screen.getByRole("button", { name: "+" }));
-    await user.click(screen.getByRole("button", { name: "-" }));
+    const toCart = screen.queryByRole("link", { name: "Cart" });
+    await act(async () => {
+      await user.click(toCart);
+    });
+
+    await act(async () => {
+      await user.click(screen.getByRole("button", { name: "+" }));
+      await user.click(screen.getByRole("button", { name: "+" }));
+      await user.click(screen.getByRole("button", { name: "-" }));
+    });
 
     expect(screen.getByTestId("number").textContent).toBe("2");
     expect(screen.getByTestId("subtotal").textContent).toBe(
@@ -62,14 +81,23 @@ describe("App Integration test", () => {
     const user = userEvent.setup();
 
     const toShop = screen.queryByRole("link", { name: "Shop" });
-    await user.click(toShop);
-    const buttons = screen.queryAllByRole("button", { name: "Add to Cart" });
-    await user.click(buttons[0]);
-    await user.click(buttons[1]);
-    const toCart = screen.queryByRole("link", { name: "Cart" });
-    await user.click(toCart);
+    await act(async () => {
+      await user.click(toShop);
+    });
 
-    await user.click(screen.getAllByRole("button", { name: "delete" })[0]);
+    const buttons = screen.queryAllByRole("button", { name: "Add to Cart" });
+    await act(async () => {
+      await user.click(buttons[0]);
+      await user.click(buttons[1]);
+    });
+    const toCart = screen.queryByRole("link", { name: "Cart" });
+    await act(async () => {
+      await user.click(toCart);
+    });
+
+    await act(async () => {
+      await user.click(screen.getAllByRole("button", { name: "delete" })[0]);
+    });
 
     expect(screen.queryAllByTestId("checkout-item").length).toBe(1);
   });
