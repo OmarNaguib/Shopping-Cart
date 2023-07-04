f0c2c64c925c39928eade18ee4c98ea9faa2e7b7
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-04 22:34:26 +0300

    Test cart addition

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { queryAllByRole, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
 import userEvent from "@testing-library/user-event";
 
-describe("Integration test", () => {
+describe("App Integration test", () => {
   it("Displays items in shop", async () => {
     render(<App />);
     const user = userEvent.setup();
@@ -13,5 +13,22 @@ describe("Integration test", () => {
     await userEvent.click(toShop);
 
     expect(screen.queryAllByTestId("card").length).toBeGreaterThan(0);
+  });
+
+  it("Adds items cart", async () => {
+    render(<App />);
+    const user = userEvent.setup();
+
+    const toShop = screen.queryByRole("link", { name: "Shop" });
+    await userEvent.click(toShop);
+    const buttons = screen.queryAllByRole("button", { name: "Add to Cart" });
+    await userEvent.click(buttons[0]);
+    await userEvent.click(buttons[2]);
+    await userEvent.click(buttons[4]);
+
+    expect(screen.queryAllByRole("button", { name: "In Cart" }).length).toBe(3);
+    const toCart = screen.queryByRole("link", { name: "Cart" });
+    await userEvent.click(toCart);
+    expect(screen.queryAllByTestId("checkout-item").length).toBe(3);
   });
 });
