cdfd478b47048a75addcfd3db9cec998d4bab916
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-05 02:57:39 +0300

    Remove unused import

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { queryAllByRole, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
 import userEvent from "@testing-library/user-event";
