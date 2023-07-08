6f948263f78ea3c84a59da9aa6585df6f80a4d56
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-08 04:13:20 +0300

    Style shop, add heading

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,7 +3,10 @@ import Cards from "./Cards";
 export default function Shop({ items, addToCart }) {
   return (
     <div className="shop">
-      <Cards items={items} addToCart={addToCart}></Cards>
+      <div className="content">
+        <h1>Equipment</h1>
+        <Cards items={items} addToCart={addToCart}></Cards>
+      </div>
     </div>
   );
 }
