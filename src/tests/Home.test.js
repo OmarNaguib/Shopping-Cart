b3cc98ea8b6a2e9d3a47041deb13fabcfb3f028e
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-07-08 07:06:30 +0300

    Update snapshots, update home test

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Home from "../components/Home";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 describe("Home page", () => {
   it("renders correctly", () => {
-    const { container } = render(<Home />);
+    const { container } = render(
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </HashRouter>
+    );
     expect(container).toMatchSnapshot();
   });
 });
